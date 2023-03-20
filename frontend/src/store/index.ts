e6d5dce3ce2f6c5537b5b91b1e5d38ae0f5ffefe
Vitor Mateus <vitor7jan@gmail.com>
2023-03-20 19:44:30 -0300

    chore(tsx): initial config of the project

---
+++ frontend/src/store/index.ts
@@ -0,0 +1,14 @@
+import { configureStore, ThunkAction, AnyAction } from "@reduxjs/toolkit";
+
+export const store = configureStore({
+  reducer: {},
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  AnyAction
+>;
