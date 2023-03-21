afbbd7cbec2ff997e22dece29bc0c4e9638996c7
Vitor Mateus <vitor7jan@gmail.com>
2023-03-20 21:18:48 -0300

    fix(store): added return with promise

--- frontend/src/services/api/IProductsApi.ts
+++ frontend/src/services/api/IProductsApi.ts
@@ -1,6 +1,6 @@
 import { IProduct } from "../../store/products/products.types";
 
 export interface IProductsApi {
-  getAllProducts(): IProduct[];
-  getProductById(id: string): IProduct;
+  getAllProducts(): Promise<IProduct[]>;
+  getProductById(id: string): Promise<IProduct>;
 }
