6aca3a526d69f0e8063b037ad6218a3eecc94b5f
Vitor Mateus <vitor7jan@gmail.com>
2023-03-21 21:13:26 -0300

    feat(component): added info into modal of product

--- frontend/src/components/CurrentProduct.tsx
+++ frontend/src/components/CurrentProduct.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Modal } from "react-bootstrap";
+import { Modal, Carousel } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "../store";
 import { cleanCurrentProductAction } from "../store/current-product/current-product.slice";
@@ -36,8 +36,35 @@ export const CurrentProduct = () => {
       <Modal.Header closeButton>
         <Modal.Title>{name}</Modal.Title>
       </Modal.Header>
-      <Modal.Body>{description}</Modal.Body>
-      <Modal.Footer>{price}</Modal.Footer>
+      <Modal.Body>
+        <Carousel>
+          {images.map((url) => {
+            return (
+              <Carousel.Item>
+                <img src={url} className="d-block w-100" alt={name} />
+                <Carousel.Caption>
+                  <span>{name}</span>
+                </Carousel.Caption>
+              </Carousel.Item>
+            );
+          })}
+        </Carousel>
+        <div className="d-flex flex-column">
+          <div>
+            <span>Descrição: {description}</span>
+          </div>
+          <div className="d-flex flex-row">
+            <span>Categoria: {category}</span>
+            <span>Material: {material}</span>
+            <span>Preço: {price}</span>
+            <span>Desconto: {discountValue * 100}</span>
+            <span>Preço com desconto: {price * (1 - discountValue)}</span>
+          </div>
+        </div>
+      </Modal.Body>
+      <Modal.Footer>
+        <button className="btn btn-primary">Adicione ao carrinho</button>
+      </Modal.Footer>
     </Modal>
   );
 };
