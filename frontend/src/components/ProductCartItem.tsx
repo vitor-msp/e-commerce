6f39a5036fed6bcbde4e952799d28994f965d1e9
Vitor Mateus <vitor7jan@gmail.com>
2023-03-22 22:12:38 -0300

    feat(cart): showing products in cart

---
+++ frontend/src/components/ProductCartItem.tsx
@@ -0,0 +1,78 @@
+import { useDispatch } from "react-redux";
+import { AppDispatch } from "../store";
+import { updateCurrentProductAction } from "../store/current-product/current-product.slice";
+import { IProduct } from "../store/products/products.types";
+import "../design/index.css";
+import { addProductToCart } from "../store/products/products.middleware";
+import { useState } from "react";
+
+export type ProductCartItemProps = {
+  product: IProduct;
+};
+
+export const ProductCartItem = ({ product }: ProductCartItemProps) => {
+  const [quantity, setQuantity] = useState<number>(1);
+  const dispatch = useDispatch<AppDispatch>();
+  const selectProduct = () => {
+    dispatch(updateCurrentProductAction(product));
+  };
+  const {
+    category,
+    description,
+    discountValue,
+    id,
+    images,
+    material,
+    name,
+    price,
+    cart,
+    supplier,
+  } = product;
+  const changeQuantity = (
+    event: React.InputHTMLAttributes<HTMLInputElement>
+  ): void => {
+    //@ts-ignore
+    setQuantity(+event.target.value);
+  };
+  return (
+    <li className="list-group-item bg-primary rounded my-2 mx-2 p-2 opacity-product">
+      <div className="d-flex flex-row">
+        <div>
+          <input
+            type="number"
+            value={quantity}
+            onChange={changeQuantity}
+            min={1}
+            step={1}
+            className="form-control"
+            style={{ maxWidth: "80px" }}
+          />
+        </div>
+        <div>
+          <img
+            src={images[0]}
+            alt={name}
+            width={160}
+            height={120}
+            className="rounded"
+            style={{ cursor: "pointer" }}
+            onClick={selectProduct}
+          />
+        </div>
+        <div className="d-flex flex-column">
+          <span className="fw-bold">Nome: {name}</span>
+          <span>Descrição: {description}</span>
+          <span className="fst-italic">Preço: {price}</span>
+        </div>
+        <div>
+          <button
+            className="btn btn-large btn-outline-danger"
+            onClick={() => {}}
+          >
+            {" x "}
+          </button>
+        </div>
+      </div>
+    </li>
+  );
+};
--- frontend/src/pages/Cart.tsx
+++ frontend/src/pages/Cart.tsx
@@ -1,10 +1,20 @@
+import { useSelector } from "react-redux";
 import { Navbar } from "../components/Navbar";
+import { ProductCartItem } from "../components/ProductCartItem";
+import { RootState } from "../store";
 
 export const Cart = () => {
+  const productsInCart = useSelector((state: RootState) =>
+    state.products.products.filter((p) => p.cart)
+  );
   return (
     <>
       <Navbar />
-      cart
+      <ul>
+        {productsInCart.map((product) => (
+          <ProductCartItem key={product.id} product={product} />
+        ))}
+      </ul>
     </>
   );
 };
