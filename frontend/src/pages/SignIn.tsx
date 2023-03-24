1dc676f35482870bed7bbf768053c4bebf619320
Vitor Mateus <vitor7jan@gmail.com>
2023-03-23 22:55:35 -0300

    feat(component): created form signup

--- frontend/src/pages/SignIn.tsx
+++ frontend/src/pages/SignIn.tsx
@@ -4,7 +4,6 @@ export const SignIn = () => {
   return (
     <>
       <Navbar />
-      SignIn
     </>
   );
 };
--- frontend/src/pages/SignUp.tsx
+++ frontend/src/pages/SignUp.tsx
@@ -4,7 +4,26 @@ export const SignUp = () => {
   return (
     <>
       <Navbar />
-      SignUp
+      <form action="" className="d-flex flex-column jusify-content-center">
+        <div className="mx-auto">
+          <label htmlFor="">E-mail:</label>
+          <input type="email" className="form-control w-auto" />
+        </div>
+        <div className="mx-auto">
+          <label htmlFor="">Senha:</label>
+          <input type="password" className="form-control w-auto" />
+        </div>
+        <div className="mx-auto">
+          <label htmlFor="">Confirmação da Senha:</label>
+          <input type="password" className="form-control w-auto" />
+        </div>
+        <div className="mx-auto my-2">
+          <input type="reset" className="btn btn-outline-primary mx-2" />
+          <button type="button" className="btn btn-primary mx-2">
+            Cadastrar
+          </button>
+        </div>
+      </form>
     </>
   );
 };
