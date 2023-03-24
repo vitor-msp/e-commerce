1eaec7f2f6989696dab1d7798908a1b2b71377b3
Vitor Mateus <vitor7jan@gmail.com>
2023-03-23 21:25:52 -0300

    feat(component): showing orders

--- frontend/src/pages/MyOrders.tsx
+++ frontend/src/pages/MyOrders.tsx
@@ -11,13 +11,36 @@ export const MyOrders = () => {
     if (orders.length === 0) {
       dispatch(getOrders());
     }
-    console.log(orders);
-  }, [orders]);
+  }, [orders, dispatch]);
 
   return (
     <>
       <Navbar />
-      MyRequests
+      <ul className="list-group">
+        {orders.map(({ date, items }) => (
+          <li
+            className="list-group-item mx-2 my-2 rounded bg-primary"
+            key={date}
+          >
+            <span>Data: {date}</span>
+            <div className="d-flex flex-row">
+              {items.map(({ name, quantity, unitPrice }) => (
+                <div className="mx-2 my-2 bg-light rounded p-2 d-flex flex-column">
+                  <span>Nome: {name}</span>
+                  <span>Quantidade: {quantity}</span>
+                  <span>
+                    Preço unitário:{" "}
+                    {unitPrice.toLocaleString("pt-br", {
+                      style: "currency",
+                      currency: "BRL",
+                    })}
+                  </span>
+                </div>
+              ))}
+            </div>
+          </li>
+        ))}
+      </ul>
     </>
   );
 };
