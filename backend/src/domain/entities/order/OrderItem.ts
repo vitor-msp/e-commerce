e2965143191cac582570e962a5b61ee001eb143e
Vitor Mateus <vitor7jan@gmail.com>
2023-03-28 21:33:58 -0300

    feat(order item): ensure not create order with blank/invalid productId/productName/unitPrice/quantity

--- backend/src/domain/entities/order/OrderItem.ts
+++ backend/src/domain/entities/order/OrderItem.ts
@@ -11,9 +11,15 @@ export class OrderItem implements IOrderItem {
   constructor(itemInput: IOrderItem) {
     if (!itemInput.supplierId) throw new OrderItemError("invalid supplierId");
     this.supplierId = itemInput.supplierId;
+    if (!itemInput.productId) throw new OrderItemError("invalid productId");
     this.productId = itemInput.productId;
+    if (!itemInput.productName) throw new OrderItemError("invalid productName");
     this.productName = itemInput.productName;
+    if (!itemInput.unitPrice || isNaN(itemInput.unitPrice))
+      throw new OrderItemError("invalid unitPrice");
     this.unitPrice = itemInput.unitPrice;
+    if (!itemInput.quantity || isNaN(itemInput.quantity))
+      throw new OrderItemError("invalid quantity");
     this.quantity = itemInput.quantity;
   }
 
--- backend/test/domain/OrderItem.test.ts
+++ backend/test/domain/OrderItem.test.ts
@@ -56,8 +56,89 @@ describe("Order Item Tests", () => {
     }).toThrow(OrderItemError);
   });
 
-  it("should not create order item with blank/invalid productId", () => {});
-  it("should not create order item with blank/invalid productName", () => {});
-  it("should not create order item with blank/invalid unitPrice", () => {});
-  it("should not create order item with blank/invalid quantity", () => {});
+  it("should not create order item with blank/invalid productId", () => {
+    expect(() => {
+      //@ts-ignore
+      new OrderItem({
+        supplierId: "supplierId",
+        productName: "product-name",
+        unitPrice: 15.62,
+        quantity: 5,
+      });
+    }).toThrow(OrderItemError);
+    expect(() => {
+      new OrderItem({
+        supplierId: "supplierId",
+        productId: "",
+        productName: "product-name",
+        unitPrice: 15.62,
+        quantity: 5,
+      });
+    }).toThrow(OrderItemError);
+  });
+
+  it("should not create order item with blank/invalid productName", () => {
+    expect(() => {
+      //@ts-ignore
+      new OrderItem({
+        supplierId: "supplierId",
+        productId: "product-id",
+        unitPrice: 15.62,
+        quantity: 5,
+      });
+    }).toThrow(OrderItemError);
+    expect(() => {
+      new OrderItem({
+        supplierId: "supplierId",
+        productId: "product-id",
+        productName: "",
+        unitPrice: 15.62,
+        quantity: 5,
+      });
+    }).toThrow(OrderItemError);
+  });
+
+  it("should not create order item with blank/invalid unitPrice", () => {
+    expect(() => {
+      //@ts-ignore
+      new OrderItem({
+        supplierId: "supplierId",
+        productId: "product-id",
+        productName: "product-name",
+        quantity: 5,
+      });
+    }).toThrow(OrderItemError);
+    expect(() => {
+      new OrderItem({
+        supplierId: "supplierId",
+        productId: "product-id",
+        productName: "product-name",
+        //@ts-ignore
+        unitPrice: "invalid",
+        quantity: 5,
+      });
+    }).toThrow(OrderItemError);
+  });
+
+  it("should not create order item with blank/invalid quantity", () => {
+    expect(() => {
+      //@ts-ignore
+      new OrderItem({
+        supplierId: "supplierId",
+        productId: "product-id",
+        productName: "product-name",
+        unitPrice: 15.62,
+      });
+    }).toThrow(OrderItemError);
+    expect(() => {
+      new OrderItem({
+        supplierId: "supplierId",
+        productId: "product-id",
+        productName: "product-name",
+        unitPrice: 15.62,
+        //@ts-ignore
+        quantity: "invalid",
+      });
+    }).toThrow(OrderItemError);
+  });
 });
