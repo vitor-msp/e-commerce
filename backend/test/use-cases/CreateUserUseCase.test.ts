188c4dcb1ae2069672aeb7fd0b8cb90011dadf3e
Vitor Mateus <vitor7jan@gmail.com>
2024-01-04 00:38:59 -0300

    test(backend): added create user use case tests

---
+++ backend/test/use-cases/CreateUserUseCase.test.ts
@@ -0,0 +1,74 @@
+import { UsersRepositoryPG } from "../../src/repositories/users/UsersRepositoryPG";
+import { CreateUserUseCase } from "../../src/use-cases/create-user/CreateUserUseCase";
+import { PasswordEncryptor } from "../../src/use-cases/utils/password-encryptor/PasswordEncryptor";
+import { CreateUserInput } from "../../src/use-cases/create-user/CreateUserInput";
+import { ApplicationError } from "../../src/errors/ApplicationError";
+
+jest.mock("../../src/repositories/users/UsersRepositoryPG");
+jest.mock("../../src/use-cases/utils/password-encryptor/PasswordEncryptor");
+
+const UsersRepositoryPGMock = UsersRepositoryPG as jest.Mock<UsersRepositoryPG>;
+const PasswordEncryptorMock = PasswordEncryptor as jest.Mock<PasswordEncryptor>;
+
+const makeSut = () => {
+  const usersRepositoryPGMock =
+    new UsersRepositoryPGMock() as jest.Mocked<UsersRepositoryPG>;
+  const passwordEncryptorMock =
+    new PasswordEncryptorMock() as jest.Mocked<PasswordEncryptor>;
+  const sut = new CreateUserUseCase(
+    usersRepositoryPGMock,
+    passwordEncryptorMock
+  );
+  return { sut, usersRepositoryPGMock, passwordEncryptorMock };
+};
+
+describe("Create User Use Case Tests", () => {
+  it("should receive created for a valid user", async () => {
+    const { sut, usersRepositoryPGMock, passwordEncryptorMock } = makeSut();
+    usersRepositoryPGMock.existsByEmail.mockResolvedValueOnce(false);
+    passwordEncryptorMock.generateHash.mockReturnValueOnce("password-hash");
+
+    const input = new CreateUserInput({
+      email: "teste@teste.com",
+      password: "teste",
+    });
+    await sut.execute(input);
+
+    expect(usersRepositoryPGMock.existsByEmail).toHaveBeenCalledTimes(1);
+    expect(usersRepositoryPGMock.existsByEmail).toHaveBeenCalledWith(
+      "teste@teste.com"
+    );
+    expect(passwordEncryptorMock.generateHash).toHaveBeenCalledTimes(1);
+    expect(passwordEncryptorMock.generateHash).toHaveBeenCalledWith("teste");
+    expect(usersRepositoryPGMock.insert).toHaveBeenCalledTimes(1);
+    expect(usersRepositoryPGMock.insert).toHaveBeenCalledWith(
+      expect.objectContaining({
+        fields: {
+          id: expect.any(String),
+          email: { email: "teste@teste.com" },
+          password: "password-hash",
+        },
+      })
+    );
+  });
+
+  it("should receive bad request when email already in use", () => {
+    const { sut, usersRepositoryPGMock, passwordEncryptorMock } = makeSut();
+    usersRepositoryPGMock.existsByEmail.mockResolvedValueOnce(true);
+
+    const input = new CreateUserInput({
+      email: "used@teste.com",
+      password: "teste",
+    });
+
+    expect(async () => await sut.execute(input)).rejects.toThrowError(
+      ApplicationError
+    );
+    expect(usersRepositoryPGMock.existsByEmail).toHaveBeenCalledTimes(1);
+    expect(usersRepositoryPGMock.existsByEmail).toHaveBeenCalledWith(
+      "used@teste.com"
+    );
+    expect(passwordEncryptorMock.generateHash).toHaveBeenCalledTimes(0);
+    expect(usersRepositoryPGMock.insert).toHaveBeenCalledTimes(0);
+  });
+});
